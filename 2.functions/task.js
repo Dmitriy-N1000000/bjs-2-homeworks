3e4f1a4284746c174a2d0d4a08dfcb5099352700
Дмитрий <konchakov.d.s@yandex.ru>
2023-06-08 13:36:03 +0300

    functions 1 completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,44 @@
 function getArrayParams(...arr) {
+	let min = Infinity;
+	let max = -Infinity;
+	let avg = 0, sum = 0;
+	/* for (let i = 0; i < arr.length; i += 1) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
 
-  return { min: min, max: max, avg: avg };
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+
+		if (arr.length == 0) {
+			avg = 0;
+		} else {
+			sum += arr[i];
+			avg = sum / arr.length;
+			avg = Number.parseFloat(avg.toFixed(2));
+		}
+	} */
+
+	min = Math.min(...arr);
+	max = Math.max(...arr);
+	
+	if (arr.length === 0) {
+		avg = 0;
+	} else {
+		sum = arr.reduce(
+			function (previousValue, currentValue) {
+				return previousValue + currentValue
+			});
+		avg = sum / arr.length;
+		avg = Number.parseFloat(avg.toFixed(2));
+	}
+	
+	return { min: min, max: max, avg: avg };
 }
+getArrayParams(7, -54, 37, 60, -31, 29, -82);
+getArrayParams(-3, 17, -6);
+getArrayParams();
 
 function summElementsWorker(...arr) {
 
