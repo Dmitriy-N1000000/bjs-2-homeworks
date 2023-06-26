4e6df6a5467d1f750ba20c3bf30de02afc7f0a7e
Дмитрий <konchakov.d.s@yandex.ru>
2023-06-26 16:02:17 +0300

    basics 1 completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let diskriminant = Math.pow(b, 2) - 4 * a * c;
+
+	if (diskriminant < 0) {
+		return arr;
+	} else if (diskriminant === 0) {
+		arr.push(- b / (2 * a));
+		return arr;
+	} else {
+		arr.push((- b + Math.sqrt(diskriminant)) / (2 * a), (- b - Math.sqrt(diskriminant)) / (2 * a));
+		return arr;
+	}
 }
+solveEquation(-5, 7, 3);
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	
 }
