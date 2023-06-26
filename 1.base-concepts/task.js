9df06872e2aa8041724968d9f82a7ff84e1c7a4d
Дмитрий <konchakov.d.s@yandex.ru>
2023-06-26 16:58:22 +0300

    basics 2, autotests, code beautify completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,27 @@
 "use strict"
+
 function solveEquation(a, b, c) {
 	let arr = [];
-	let diskriminant = Math.pow(b, 2) - 4 * a * c;
+	let discriminant = b ** 2 - 4 * a * c;
 
-	if (diskriminant < 0) {
+	if (discriminant < 0) {
 		return arr;
-	} else if (diskriminant === 0) {
-		arr.push(- b / (2 * a));
+	} else if (discriminant === 0) {
+		arr.push(-b / (2 * a));
 		return arr;
 	} else {
-		arr.push((- b + Math.sqrt(diskriminant)) / (2 * a), (- b - Math.sqrt(diskriminant)) / (2 * a));
+		arr.push((-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a));
 		return arr;
 	}
 }
 solveEquation(-5, 7, 3);
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	
+	let monthPercent = percent / 100 / 12;
+	let S = amount - contribution;
+	let monthPayment = S * (monthPercent + (monthPercent / ((Math.pow(1 + monthPercent, countMonths)) - 1)));
+	let mustPay = parseFloat((monthPayment * countMonths).toFixed(2));
+
+	return mustPay;
 }
+calculateTotalMortgage(10, 0, 50000, 12);
