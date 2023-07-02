d1d88f2777ee0fd57dc2877bb319e7418133d2d0
Дмитрий <konchakov.d.s@yandex.ru>
2023-07-02 23:06:38 +0300

    exception-closure 1, 2, autotests completed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(valueToParse) {
+	let parsed = Number.parseFloat(valueToParse);
+	if (isNaN(parsed)) {
+		throw new Error('Невалидное значение');
+	}
+	return parsed;
+}
+
+function validateCount(valueToParse) {
+	try {
+		return parseCount(valueToParse);
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		if (a + b < c  || a + c < b || b + c < a) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+	}
+
+	get perimeter() {
+		return Number(this.a + this.b + this.c);
+	}
+
+	get area() {
+		let p = this.perimeter / 2;
+		let s = Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+		return s;
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			get perimeter() {
+				return "Ошибка! Треугольник не существует"
+			},
+			get area() {
+				return "Ошибка! Треугольник не существует"
+			}
+		}
+	}
+}
